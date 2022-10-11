d59ec9ed48b225ef9e94c58b77bb2a5ba2979aa6
Kyuwoong Lee <kwooler@gmail.com>
2022-10-10 22:08:19 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+
+## Assignment 5-2:Lab 5-9-1
+
+<!--
+### [Question 3](https://docs.google.com/document/d/1iMc8wbFr1h2pPzT8vaulgZehNN2hZANDZFNxekmzUq0/edit#bookmark=id.v7ydzultqkvr)
+-->
+
+
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include        <iostream>
+#include        <iomanip>
+using   namespace       std;
+
+int main()
+{
+    int min, number;
+
+	// complete your code
+  
+    cout << "The min value is " << min << endl;
+}
