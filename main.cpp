abb57831857a0226f1565a51e304c0e3579a224e
Kyuwoong Lee <kwooler@gmail.com>
2023-09-17 15:18:45 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,9 @@ int main()
 {
     int i, begin, end, sum=0 ;
 
+	/* ************************************************
 	// complete your code
+	*********************************************** */
   
     cout << "The sum of even values is " << sum << endl;
 }
