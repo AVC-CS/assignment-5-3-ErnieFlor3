f2f89bcb1a5154b8da83266c046499225f94d9c8
Ernesto Flores <eflores73@students.avc.edu>
2024-02-20 12:28:10 -0800

    Finished

--- main.cpp
+++ main.cpp
@@ -10,16 +10,18 @@ int main()
 	// complete your code
 	*********************************************** */
 
-		cout << "Enter two numbers (first must be less than the second): ";
-		cin >> begin >> end;
-
+		begin = 1; 
+		end = 0;
 	 	while(begin >= end) {
+			cout << "Enter two numbers (first must be less than the second): ";
+			cin >> begin >> end;
+		}
 
 	for (int i = begin; i <= end; i++) {
 		if (i % 2 == 0) {
 			sum += i;
 		}
 	}
-		}
+		
     cout << "The sum of even values is " << sum << endl;
 }
